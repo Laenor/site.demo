d67cac80265e8b9d3be0856793f96b69c0b0e2a5
Laenor <baptiste.predhomme@gmail.com>
2019-11-10 23:26:36 +0100

    add Pagination

--- app/Views/liste.php
+++ app/Views/liste.php
@@ -1,18 +1,22 @@
 <!doctype html>
 
-<html>
+<html lang="fr">
     <head>
+        <title>Demo consommation API</title>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url('css/style.css'); ?>">
+        <link type="text/css" rel="stylesheet" href="<?php echo base_url('css/simplePagination.css'); ?>"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="<?php echo base_url('js/jquery.simplePagination.js'); ?>"></script>
         <script type="text/javascript" src="<?php echo base_url('js/script.js'); ?>"></script>
     </head>
     <body>
     <header>
-        <title>Demo consommation API</title>
         <h1>Demo consommation API</h1>
     </header>
     <main>
-        <div id="content-wrap">
+        <div id="pagination">
+        </div>
+        <div id="content">
         </div>
     </main>
     <footer>
--- public/js/script.js
+++ public/js/script.js
@@ -7,9 +7,8 @@ $(document).ready(function() {
                 dataType: 'json',
                 data: {data_action: 'fetch_all'},
                 success: function (data) {
-                    $.each(data, function(i, values){
-                           console.log(data[i]['budget_demande']);
-                        $('#content-wrap').append(
+                    $.each(data, function(i){
+                        $('#content').append(
                             "<article>" +
                                 "<div class='titre'>" +
                                     "<p>"+data[i]['titre_demande']+"</p>" +
@@ -17,13 +16,24 @@ $(document).ready(function() {
                                     "<p>"+data[i]['date_demande']+"</p>" +
                                 "</div>" +
                                 "<p>"+data[i]['description_demande']+"</p>" +
-                            "</article>");
+                            "</article>"
+                        );
+                    });
+                    $("article").slice(6).hide();
+                    $(function() {
+                        $('#pagination').pagination({
+                            items: data.length,
+                            itemsOnPage: 6,
+                            cssStyle: 'compact-theme',
+                            onPageClick: function(pageNumber) {
+                                $("article").hide().slice((pageNumber-1)*6,pageNumber*6).show();
+                            }
+                        });
                     });
                 },
                 error:function () {
                     alert("echec");
                 }
             });
-
     }
 );
